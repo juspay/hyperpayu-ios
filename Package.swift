bfa7643f0de2b0f3f640e3d16e3e04bcd9c544dc
Balaganesh S <balaganesh@juspay.in>
2024-10-29 19:23:45 +0530

    Updated Package for version 2.2.1.12

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "HyperPayU",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "HyperPayU",
+            targets: ["HyperPayU", "HyperPayUDependencies"]
+        )
+    ],
+    dependencies: [
+        .package(name: "PayUIndia-3DS2-SDK", url: "https://github.com/payu-intrepos/PayU3DS2SDK-iOS.git", .exact("1.4.2"))
+    ],
+    targets: [
+        .binaryTarget(
+            name: "HyperPayU",
+            url: "https://public.releases.juspay.in/release/ios/hyper-sdk/2.2.1.12/HyperPayU.zip",
+            checksum: "f4ad9f818570058aca0d0ac6b9c01dcce68c3feff9f9749da65c8b3e6ed3bee9"
+        ),
+        .target(
+            name: "HyperPayUDependencies",
+            dependencies: [
+                .product(name: "PayUIndia-3DS2-SDK", package: "PayUIndia-3DS2-SDK")
+            ]
+        )
+    ]
+)
